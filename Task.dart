2f19a72d8b2292cdfff23e1ed6b3b3d19a7fcba6
Nubla07 <afifamuntaha07@gmail.com>
2024-03-11 02:53:49 +0600

    Assignment-1

---
+++ Task.dart
@@ -0,0 +1,21 @@
+class Media {
+  void play() {
+    print('Playing media... ');
+  }
+}
+
+class Song extends Media {
+  String artist;
+  Song(this.artist);
+  @override
+  void play() {
+    print('Playing song by $artist...');
+  }
+}
+
+void main() {
+  Media mediaInstance = Media();
+  Song songInstance = Song('Shreya Ghoshal');
+  mediaInstance.play();
+  songInstance.play();
+}
